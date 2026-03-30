853e5c838a7dc71b9895cb74ffbc39b6022a1495
Kartik Jain <koderkartik@gmail.com>
2026-03-31 00:55:16 +0530

    deployment work

--- server/index.js
+++ server/index.js
@@ -1,22 +1,47 @@
-const dns = require('dns')
-dns.setServers(['8.8.8.8', '8.8.4.4'])
+const dns = require("dns");
 
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 dotenv.config();
+
+
 connectDB();
 
 const app = express();
-app.use(cors());
+
+
+
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/sessions', require('./routes/sessions'));
 
-app.get('/', (req, res) => res.send('Vi-Notes API running'));
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/sessions", require("./routes/sessions"));
+
+
+app.get("/", (req, res) => {
+  res.status(200).send("Vi-Notes API running 🚀");
+});
+
+
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
