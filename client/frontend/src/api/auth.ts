a0c2cf0246bf10c435d4685ec95e75ef6326062a
Kartik Jain <koderkartik@gmail.com>
2026-03-31 01:42:45 +0530

    Finally

--- client/frontend/src/api/auth.ts
+++ client/frontend/src/api/auth.ts
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
+const API = import.meta.env.VITE_API_URL;
+
 export const registerUser = async (username: string, email: string, password: string) => {
-  const { data } = await axios.post('/api/auth/register', { username, email, password })
+  const { data } = await axios.post(`${API}/api/auth/register`, { username, email, password })
   return data
 }
 
 export const loginUser = async (email: string, password: string) => {
-  const { data } = await axios.post('/api/auth/login', { email, password })
+  const { data } = await axios.post(`${API}/api/auth/login`, { email, password })
   return data
 }
